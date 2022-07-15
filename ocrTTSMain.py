2e3391e99d0f1f4532eea68cfcbde4e5fb4ff910
emoairx <emoairx@gmail.com>
2022-07-15 20:58:34 +0800

    调参

--- ocrTTSMain.py
+++ ocrTTSMain.py
@@ -6,33 +6,34 @@ from ttsRead import ttsRead
 from difflib import SequenceMatcher
 import pyautogui
 import numpy as np
-import copy
+# import copy
 
-forMoreDetails = True
+# forMoreDetails = True
 
-#image = cv2.imread("./genshin.png")
-#im0 = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
-#im0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+# image = cv2.imread("./genshin.png")
+# im0 = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+# im0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+
 
 print('正在启动服务')
 ttsRead('正在启动服务')
 
 reader = PaddleOCR(use_angle_cls=True, lang="ch", use_gpu=False)
 
-def isNotASCII(s: str) -> bool:
-    rate = 0
-    for x in s:
-        if ord(x) <= 127:
-            rate += 1
-    rate /= len(s)
-    return rate < 4/5
+# def isNotASCII(s: str) -> bool:
+#     rate = 0
+#     for x in s:
+#         if ord(x) <= 127:
+#             rate += 1
+#     rate /= len(s)
+#     return rate < 4/5
 
 
-lastStr = ''
+# lastStr = ''
 
 
-def isDifferent(s: str) -> bool:
-    return SequenceMatcher(None, lastStr, s).ratio() < .8
+# def isDifferent(s: str) -> bool:
+#     return SequenceMatcher(None, lastStr, s).ratio() < .8
 
 
 def getRegion(status: str) -> 'tuple[int, int, int, int]':
@@ -41,9 +42,9 @@ def getRegion(status: str) -> 'tuple[int, int, int, int]':
     if isDetecting:
         return (0, 0, rect[0]*2//10, rect[1]*2//10)
     else:
-        left = rect[0]//20
+        left = rect[0] * 0.17
         right = rect[0]-left
-        top = rect[1]//3
+        top = rect[1]//4
         return (left, top, right-left, rect[1]-top)
 
 
@@ -64,17 +65,18 @@ def get_image(status):
     im0 = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
     lower_color = color[status]
     upper_color = color[status]
-    im0 = cv2.inRange(im0, lower_color, upper_color)  # inrange函数将根据最小最大HSV值检测出自己想要的颜色部分
-#    ret, im0 = cv2.threshold(im0, 240, 255, type=cv2.THRESH_TOZERO)
-#    im0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+    im0 = cv2.inRange(im0, lower_color, upper_color)
+    # inrange函数将根据最小最大HSV值检测出自己想要的颜色部分
+    # ret, im0 = cv2.threshold(im0, 240, 255, type=cv2.THRESH_TOZERO)
+    # im0 = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
     
     return im0
 
 def get_ocr_word(image):
-    image_shrink = cv2.resize(image, (0, 0), fx=.5, fy=.5)
+    image_shrink = cv2.resize(image, (0, 0), fx=.7, fy=.7)
     result = reader.ocr(image_shrink)
     result = [x[1][0] for x in result]
-    resultStr = '\n'.join(result)
+    resultStr = ' '.join(result)
     return resultStr
 
 while True:
@@ -84,7 +86,7 @@ while True:
         #用于判断是否存在“自动”两个字
         resultStr = get_ocr_word(now_image)
         print('detecting -- ',resultStr)
-        if resultStr.count('自动') == 0:
+        if resultStr.count('Auto') == 0:
             print('获取到了非关键数据')
             time.sleep(0.5)
         else:
@@ -112,9 +114,10 @@ while True:
                 print(result)
                 ttsRead(result)
             Current_str = ''
+            time.sleep(0.2)
         else:
             Current_str += get_ocr_word(delta_image)
             print('检测到字幕仍然在滚动，当前检测到文本为：',Current_str)
 
     pre_image = now_image
-    laststatus = status
+    # laststatus = status
